47873c2743a14e71b6e86692dba3e2418c7b368f
coder-pig <511810970@qq.com>
2017-10-22 14:56:46 +0800

    更新后台api

---
+++ XWebAPIDemo/X_1_FirstWebAPI/Controller/ArticleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Http;
+using X_1_FirstWebAPI.Model;
+using System.Data;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+
+namespace X_1_FirstWebAPI.Controller
+{
+    public class ArticleController : ApiController
+    {
+        [HttpGet]
+        public string getAricle() {
+            DBClass db = new DBClass();
+            SqlConnection conn = db.GetConnection();
+            string sql = "select * from tb_article";
+            DataSet dataSet = new DataSet();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sql, conn);
+            sqlDataAdapter.Fill(dataSet);
+            return JsonConvert.SerializeObject(dataSet);
+        }
+    }
+}
---
+++ XWebAPIDemo/X_1_FirstWebAPI/Model/Navs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace X_1_FirstWebAPI.Model
+{
+    public class Navs
+    {
+        public int Id { get; set; }
+        public string UName { get; set; }
+        public int UAge { get; set; }
+        public string UAddress { get; set; }
+    }
+}
